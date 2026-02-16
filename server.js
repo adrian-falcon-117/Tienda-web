d86814bdae988a0abd5f7b30157f52263fa9a637
Alex-Falcon117 <falex9218@gmail.com>
2026-02-16 12:15:35 -0300

    Modificacion en server.js

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(express.static(path.join(__dirname, "dist")));
 // Rutas personalizadas 
 app.get("/admin", (req, res) => { res.sendFile(path.join(__dirname, "dist", "admin.html")); }); 
 app.get("/login", (req, res) => { res.sendFile(path.join(__dirname, "dist", "login.html")); });
-
-// Fallback para SPA (si usas React Router o rutas dinámicas)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en puerto ${port}`);
